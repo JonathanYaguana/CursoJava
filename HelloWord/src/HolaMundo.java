b8ee4d842ff298cd8b3d3a1729cce3a8fcfb5c01
JonathanYaguana <jonathansacrox93@gmail.com>
2024-12-07 23:47:46 -0500

    Ejemplos de variables

--- HelloWord/src/HolaMundo.java
+++ HelloWord/src/HolaMundo.java
@@ -1,6 +1,19 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde Java");
-        System.out.println("Soy Jonathan Yaguana");
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+
+        boolean valor = true;
+        int numero2 = 10;
+        if( valor ) {
+            System.out.println( "numero = " + numero );
+            numero2 = 10;
+        }
+        System.out.println( "numero2 = " + numero2 );
+
+        var numero3 = "100";
     }
 }
